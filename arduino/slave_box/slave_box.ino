acb15ade53c5d6edb3b4e3a59ecd8513d8143aca
icovada <federico@tabbo.it>
2018-10-25 16:59:42 +0200

    First working base slave version

--- arduino/slave_box/slave_box.ino
+++ arduino/slave_box/slave_box.ino
@@ -1,3 +1,45 @@
+#include "painlessMesh.h"
+
+#define   MESH_PREFIX     "whateverYouLike"
+#define   MESH_PASSWORD   "somethingSneaky"
+#define   MESH_PORT       5555
+
+painlessMesh  mesh;
+
+void sendMessage() {
+  String msg = "Hello from node ";
+  msg += mesh.getNodeId();
+  mesh.sendBroadcast( msg );
+}
+
+void receivedCallback( uint32_t from, String &msg ) { 
+  int pinNumber = msg.substring(0,1).toInt();
+  String colour = msg.substring(1);
+  bool onoff;
+
+  Serial.println(pinNumber);
+  Serial.println(colour);
+
+  if (colour == "FFFFFF"){
+    onoff = 1;
+  } else {
+    onoff = 0;
+  }
+
+  digitalWrite(pinNumber, onoff);
+
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+}
+
+void changedConnectionCallback() {
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+}
+
+
 class Input{
   int inPin;
   bool oldpin;
@@ -18,11 +60,11 @@ class Input{
       if (!digitalRead(inPin) && oldpin){
         Serial.print("PRESSED ");
         Serial.println(inPin);
-        oldpin = 0
+        oldpin = 0;
         debounce = millis();
       }
       if (digitalRead(inPin) && !oldpin){
-        oldpin = 1
+        oldpin = 1;
         debounce = millis();
       }
     }
@@ -36,9 +78,15 @@ Input di2(5);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
+  mesh.init( MESH_PREFIX, MESH_PASSWORD, MESH_PORT );
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
 }
 
 void loop() {
   di1.Check();
   di2.Check();
+  mesh.update();
 }
